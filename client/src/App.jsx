7e0d869d722856f6391cea3ad9c017cc6f39341f
Pedro <pedrotenerelli257@gmail.com>
2024-01-24 18:14:04 -0300

    feat: add requirements

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <div className="p-8 text-red-500">
+          salveee
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
